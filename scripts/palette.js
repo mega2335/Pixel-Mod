37c6f8d739959e8443b74f0252e56b39c09729c6
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-25 13:09:36 -0700

    Create palette.js

---
+++ scripts/palette.js
@@ -0,0 +1,10 @@
+function col(color){
+  return Color.valueOf(color)
+};
+exports.col = col;
+
+const brownLight = col("8c6e6e");
+exports.brownLight = brownLight;
+
+const brownDark = col("6e6666");
+exports.brownDark = brownDark;
