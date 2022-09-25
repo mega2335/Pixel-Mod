88b82b3e220cfdcadfe6c8064d0fe2a91f99ae9f
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-25 13:31:23 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -1,18 +1,11 @@
 const ef = require("effects");
 const bul = require("bullets");
 const c = require("consts");
-/*
+
 const bloom = extend(PowerTurret, "bloom", {
-  shootEffect: ef.smallShoot,
-  smokeEffect: Fx.none,
-  shootType: bul.bloomPixel,
-  range: c.bloomRange,
-});
-*/
-const bloom = extend(PowerTurret, "bloom", {
-  shootSound: Sounds.bigshot, // Now's your chance to be a [[BIG SHOT]]
   shootType: bul.bloomPixel,
   shootEffect: ef.smallShoot,
   smokeEffect: Fx.none,
   range: c.bloomRange,
 });
+exports.bloom = bloom;
