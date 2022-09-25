5c78df936faba471787863935eb086ca40dd95a0
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-25 14:33:56 -0700

    Update effects.js

--- scripts/effects.js
+++ scripts/effects.js
@@ -6,7 +6,7 @@ const smallShoot = new Effect(50, e => {
 		Fill.rect(e.x + x, e.y + y, 2, 2);
 	}});
 	
-	Angles.randLenVectors(e.id, 5, e.finpow() * 20.0, e.rotation, 30.0, hl);
+	Angles.randLenVectors(e.id, 8, e.finpow() * 20.0, e.rotation, 30.0, hl);
 });
 exports.smallShoot = smallShoot;
 
@@ -30,3 +30,17 @@ const revampHit = new Effect(45, e =>{
 	Lines.circle(e.x, e.y, 4 + e.finpow() * 20);
 });
 exports.revampHit = revampHit;
+
+const smallHealShoot = new Effect(50, e => {
+	Draw.color(pal.brownLight, pal.brownDark, e.fin());
+	const hl = new Floatc2({get: function(x, y){
+		Fill.rect(e.x + x, e.y + y, 2, 2);
+	}});
+	
+	Angles.randLenVectors(e.id, 8, e.finpow() * 20.0, e.rotation, 30.0, hl);
+	
+	Lines.stroke(e.fout() * 2);
+	Lines.square(e.x, e.y, 7, Time.time * 2);
+	Lines.square(e.x, e.y, 7, Time.time * -2);
+});
+exports.smallHealShoot = smallHealShoot;
