e732a90d1d545dd9a27d1e9fc44216a28d89c627
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-25 13:58:15 -0700

    Update effects.js

--- scripts/effects.js
+++ scripts/effects.js
@@ -9,3 +9,13 @@ const smallShoot = new Effect(50, e => {
 	Angles.randLenVectors(e.id, 5, e.finpow() * 20.0, e.rotation, 30.0, hl);
 });
 exports.smallShoot = smallShoot;
+
+const bloomTrail = new Effect(20, e =>{
+	Draw.color(pal.brownLight, pal.brownDark, e.fin());
+	Fill.rect(e.x, e.y, 2, 2);
+});
+
+const revampTrail = new Effect(20, e =>{
+	Draw.color(pal.greenLight, pal.greenDark, e.fin());
+	Fill.rect(e.x, e.y, 3.5, 3.5);
+});
