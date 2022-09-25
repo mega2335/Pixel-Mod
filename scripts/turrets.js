72fa11dba02ca28d6b5649866dd380bcb555c221
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-25 13:25:04 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -1,10 +1,11 @@
 const ef = require("effects");
 const bul = require("bullets");
 const c = require("consts");
-
+/*
 const bloom = extend(PowerTurret, "bloom", {
   shootEffect: ef.smallShoot,
   smokeEffect: Fx.none,
   shootType: bul.bloomPixel,
   range: c.bloomRange,
 });
+*/
