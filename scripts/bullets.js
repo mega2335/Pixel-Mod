c4e2b876613a416df83a35767e9ab9af00c551df
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-26 06:54:37 -0700

    Update bullets.js

--- scripts/bullets.js
+++ scripts/bullets.js
@@ -150,3 +150,21 @@ const knifePixel = extend(BasicBulletType, {
   trailInterval: 0.4,
 });
 exports.knifePixel = knifePixel;
+
+const knifeRail = extend(PointBulletType, {
+	damage: 0,
+  hitEffect: ef.knifeHit,
+  despawnEffect: Fx.none,
+  despawnHit: true,
+  splashDamage: 210,
+  splashDamageRadius: 10,
+  speed: c.knifeRange + (c.block * 1.5),
+  draw(b){
+    Draw.color(pal.orangeLight)
+    Draw.z(Layer.bullet)
+    Fill.rect(b.x, b.y, 4, 4)
+  },
+  trailEffect: ef.knifeRailTrail,
+  trailInterval: 4,
+});
+exports.knifePixel = knifePixel;
