8b8d93f92ef207ebe5528062af141f8e9bc02112
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-25 18:19:58 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -14,6 +14,48 @@ needle.ammo(
 )
 exports.needle = needle;
 
+const blade = extend(ItemTurret, "blade", {
+  shootEffect: ef.bigShoot,
+  smokeEffect: ef.smallSmoke,
+  range: c.bladeRange,
+  setStats(){
+    this.super$setStats();
+    this.stats.add(Stat.ammo, StatValues.ammo(ObjectMap.of(this, bul.bladeLaser)));
+  },
+});
+blade.buildType = () => extend(PowerTurret.PowerTurretBuild, blade, {
+	//reload of attack
+creload : 0,
+  trns: new Vec2(),
+  trns2: new Vec2(),
+    updateTile(){
+        this.super$updateTile();
+        
+        if(this.isShooting() && this.isActive() && this.hasAmmo() && this.creload >= 14){
+            this.creload = 0
+          this.trns2.trns(this.rotation, this.shootY)
+          //vector
+          this.trns.trns(this.rotation, -2)
+            //bullet creating
+            bul.bladeLaser.create(this, this.team, (this.x + this.trns.x) + this.trns2.x, (this.y + this.trns.y) + this.trns2.y, this.rotation)
+          //vector
+          this.trns.trns(this.rotation, 2)
+            //bullet creating
+            bul.bladeLaser.create(this, this.team, (this.x + this.trns.x) + this.trns2.x, (this.y + this.trns.y) + this.trns2.y, this.rotation)
+            //shoot sound
+            Sounds.bigshot.at(this)
+            
+        }
+        else{
+            if(this.creload < 14){this.creload += 1} 
+        }
+    },
+});
+blade.ammo(
+  Items.graphite, bul.bladeGraphite
+)
+exports.blade = blade;
+
 const bloom = extend(PowerTurret, "bloom", {
   shootType: bul.bloomPixel,
   shootEffect: ef.smallShoot,
