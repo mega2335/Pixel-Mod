19172688e60b0a78c63b0119afb69363958db2c9
Mega <110144856+mega2335@users.noreply.github.com>
2022-10-10 06:51:11 -0700

    Update DrawPixelParticles.js

--- scripts/libs/drawers/DrawPixelParticles.js
+++ scripts/libs/drawers/DrawPixelParticles.js
@@ -1,6 +1,4 @@
 exports.newDrawer = function newDrawer(obj){
-  obj = JavaAdapter.apply(DrawParticles, obj);
-  
   const color = obj.color;
   const particles = obj.particles
   const particleLife = obj.particleLife, particleRad = obj.particleRad, particleSize = obj.particleSize;
