cbba18b78ef92045e1c7fb07847700241af0bc77
Mega <110144856+mega2335@users.noreply.github.com>
2022-10-10 06:52:05 -0700

    a

--- scripts/production.js
+++ scripts/production.js
@@ -4,6 +4,6 @@ const c = require("consts");
 const ef = require("effects");
 
 const pixellitePress = extend(GenericCrafter, "pixellite-press", {
-  drawer: new DrawMulti(new DrawRegion("bottom"), DrawPixelParticles.newDrawer({color: pal.lightOrange, particles: 20, particleLife: 50, particleRad: 4, particleSize: 2, reverse: true, interp: new PowIn(1.5), alpha: 0.8, blending: Blending.normal}), new DrawRegion("top")),
+  drawer: new DrawMulti(new DrawRegion("bottom"), DrawPixelParticles.newDrawer({color: pal.orangeLight, particles: 20, particleLife: 50, particleRad: 4, particleSize: 2, reverse: true, interp: new PowIn(1.5), alpha: 0.8, blending: Blending.normal}), new DrawRegion("top")),
   craftEffect: Fx.none,
 });
